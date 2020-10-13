84dfb082641b65d7e3e451dcc0c790784a170c31
csjohn1 <csjohn@shaw.ca>
2020-10-13 17:09:52 +0000

    submitting 2queries

---
+++ 2_queries_joins/studur.sql
@@ -0,0 +1,5 @@
+SELECT sum(duration) as total_duration
+FROM students
+FULL OUTER JOIN assignment_submissions
+ON students.id=assignment_submissions.student_id
+WHERE students.name='Ibrahim Schimmel'
